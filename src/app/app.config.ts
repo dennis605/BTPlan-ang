9b27cc8aa73df491a74ab64d87bdf918a02e3a91
dennis605605 <dennis605605@gmail.com>
2024-12-24 17:28:42 +0000

    feat: Deutsche Lokalisierung hinzugefügt (LOCALE_ID auf de-DE gesetzt)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    { provide: LOCALE_ID, useValue: 'de-DE' }
   ]
 };
